313852b087a7e7838ea3f5e22e232b8b8c59fe6e
admin <admin@itheima.com>
2019-03-30 14:42:49 +0800

    默认的

---
+++ git-test/src/com/itheima/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
